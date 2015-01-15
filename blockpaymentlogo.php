8d38b1eecc56ccf8187c32a3610c194ad50d50d2
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-01-15 18:42:29 +0100

    // Version updated to v0.3.3

--- blockpaymentlogo.php
+++ blockpaymentlogo.php
@@ -33,7 +33,7 @@ class BlockPaymentLogo extends Module
 	{
 		$this->name = 'blockpaymentlogo';
 		$this->tab = 'front_office_features';
-		$this->version = '0.3.2';
+		$this->version = '0.3.3';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
