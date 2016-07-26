a4cb926a53378de835ebbb52ccdb7abee4f148b7
Maxime Biloé <maxime.biloe@gmail.com>
2016-07-26 11:46:08 +0200

    // Version updated to v0.4.1

--- blockpaymentlogo.php
+++ blockpaymentlogo.php
@@ -33,7 +33,7 @@ class BlockPaymentLogo extends Module
 	{
 		$this->name = 'blockpaymentlogo';
 		$this->tab = 'front_office_features';
-		$this->version = '0.4.0';
+		$this->version = '0.4.1';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
